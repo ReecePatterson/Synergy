9d773e8c0b8b9ddbbc774faa93391a5c9a27ab61
Emmanuel Godinez <manuel.godinez22@gmail.com>
2014-08-26 22:10:16 +1200

    Some changes

--- RiverWatch Windows Phone App/RiverWatch Windows Phone App/MainPage.xaml.cs
+++ RiverWatch Windows Phone App/RiverWatch Windows Phone App/MainPage.xaml.cs
@@ -20,9 +20,9 @@ namespace RiverWatch_Windows_Phone_App
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class MainPage : Page
     {
-        public BlankPage1()
+        public MainPage()
         {
             this.InitializeComponent();
         }
@@ -35,5 +35,6 @@ namespace RiverWatch_Windows_Phone_App
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+
     }
 }
