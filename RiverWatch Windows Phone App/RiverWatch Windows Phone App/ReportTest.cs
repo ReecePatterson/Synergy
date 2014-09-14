56abe1bc5f4a441e2171e4117b23c2b0e6e7f2ec
samuraiRAZR <alvinpascual21@ymail.com>
2014-09-14 22:19:06 +1200

    Initial Commit of Text

---
+++ RiverWatch Windows Phone App/RiverWatch Windows Phone App/ReportTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace RiverWatch_Windows_Phone_App
+{
+    [TestClass]
+    class ReportTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+         
+        }
+    }
+}
