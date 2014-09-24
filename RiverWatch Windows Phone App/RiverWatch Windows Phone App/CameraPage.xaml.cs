25f97a30fecb75ea0a8863aa517a9c4e4855dd45
Emmanuel <manuel.godinez22@gmail.com>
2014-09-24 15:43:19 +1200

    camera

--- RiverWatch Windows Phone App/RiverWatch Windows Phone App/CameraPage.xaml.cs
+++ RiverWatch Windows Phone App/RiverWatch Windows Phone App/CameraPage.xaml.cs
@@ -81,22 +81,18 @@ namespace RiverWatch_Windows_Phone_App
                     case SimpleOrientation.NotRotated:
                         //Portrait Up 
                         cameraButton.RenderTransform = new RotateTransform() { Angle = 0 };
-                        mediaCapture.SetPreviewRotation(VideoRotation.None);
                         break;
                     case SimpleOrientation.Rotated90DegreesCounterclockwise:
                         //LandscapeLeft 
                         cameraButton.RenderTransform = new RotateTransform() { Angle = 90 };
-                        mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
                         break;
                     case SimpleOrientation.Rotated180DegreesCounterclockwise:
                         //PortraitDown 
                         cameraButton.RenderTransform = new RotateTransform() { Angle = 180 };
-                        mediaCapture.SetPreviewRotation(VideoRotation.Clockwise180Degrees);
                         break;
                     case SimpleOrientation.Rotated270DegreesCounterclockwise:
                         //LandscapeRight 
                         cameraButton.RenderTransform = new RotateTransform() { Angle = 270 };
-                        mediaCapture.SetPreviewRotation(VideoRotation.Clockwise270Degrees);
                         break;
                     case SimpleOrientation.Faceup:
                        // txtOrientation.Text = "Faceup";
@@ -192,7 +188,7 @@ namespace RiverWatch_Windows_Phone_App
             //await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.Photo, maxResolution);
 
 
-            mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
+            //mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
             CameraPreview.Source = mediaCapture;
             await mediaCapture.StartPreviewAsync();
         }
