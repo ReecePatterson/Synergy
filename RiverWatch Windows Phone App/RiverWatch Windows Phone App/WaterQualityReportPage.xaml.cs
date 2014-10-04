58e820a8f5c00f0039a415a061fa69dbf38e43a9
Emmanuel <manuel.godinez22@gmail.com>
2014-10-04 15:42:46 +1300

    BT RFCOMM

--- RiverWatch Windows Phone App/RiverWatch Windows Phone App/WaterQualityReportPage.xaml.cs
+++ RiverWatch Windows Phone App/RiverWatch Windows Phone App/WaterQualityReportPage.xaml.cs
@@ -30,6 +30,10 @@ namespace RiverWatch_Windows_Phone_App {
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class WaterQualityReportPage : Page {
+
+        ObservableCollection<PeerInformation> _pairedDevices;  // a local copy of paired device information
+        StreamSocket _socket; // socket object used to communicate with the device
+
         public WaterQualityReportPage() {
             this.InitializeComponent();
             HardwareButtons.BackPressed += HardwareButtons_BackPressed;
@@ -45,141 +49,171 @@ namespace RiverWatch_Windows_Phone_App {
             }
         }
 
+        BluetoothLEDevice currentDevice { get; set; }
+        string deviceName = "SGS4";
         protected async override void OnNavigatedTo(NavigationEventArgs e) {
+            _pairedDevices = new ObservableCollection<PeerInformation>();
+            PairedDevicesList.ItemsSource = _pairedDevices;
 
+            RefreshPairedDevicesList();
         }
 
+        private async Task<Boolean> connectToPoos() {
+            PeerFinder.AllowBluetooth = true;
+            PeerFinder.Start();
+
+            PeerFinder.AlternateIdentities["Bluetooth:PAIRED"] = "";
+            var available_devices = await PeerFinder.FindAllPeersAsync();
+            PeerInformation pi = null;
+            if (available_devices.Count == 0) {
+                return false;
+            }
+            else {
+                for (int i = 0; i < available_devices.Count; i++) {
+                    pi = available_devices[i];
+                    this.thingsFound.Text = "\nPaired Device Name: " + pi.DisplayName;
+                    if (pi.DisplayName.Contains("Emmanuel")) {
+                        this.thingsFound.Text += "\nEmans device found";
+                        StreamSocket socket = new StreamSocket();
+
+                        if (socket != null) {
+                            socket.Dispose();
+                        }
+
+                        this.thingsFound.Text += "\nSocket Created with hostname: " + pi.HostName + " \nwith service name: " + pi.ServiceName;
+                        //await socket.ConnectAsync(new End);
+                        await socket.ConnectAsync(pi.HostName, "2");
+                        this.thingsFound.Text += "\nDevice Connected";
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+
+        }
 
         private void ReturnButton_Click(object sender, RoutedEventArgs e) {
             Frame.Navigate(typeof(HubPage));
         }
 
 
-        private async void ScanDevices() {
-            lstDevices.Items.Clear();
-            string genericUUID = GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.GenericAccess);
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e) {
+            //RefreshPairedDevicesList();
 
-            foreach (DeviceInformation di in await DeviceInformation.FindAllAsync(genericUUID)) {
-                BluetoothLEDevice bleDevice = await BluetoothLEDevice.FromIdAsync(di.Id);
-                lstDevices.Items.Add(new MyBluetoothLEDevice(bleDevice));
+        }
+
+        private async void RefreshPairedDevicesList() {
+            try {
+                // Search for all paired devices
+                PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+                var peers = await PeerFinder.FindAllPeersAsync();
+
+                // By clearing the backing data, we are effectively clearing the ListBox
+                _pairedDevices.Clear();
+
+                if (peers.Count == 0) {
+                    //MessageBox.Show(AppResources.Msg_NoPairedDevices);
+                }
+                else {
+                    // Found paired devices.
+                    foreach (var peer in peers) {
+                        _pairedDevices.Add(peer);
+                    }
+                }
             }
-
-        }
-
-        BluetoothLEDevice currentDevice { get; set; }
-
-        private void lstDevices_Tapped(object sender, TappedRoutedEventArgs e) {
-            currentDevice = (lstDevices.SelectedItem as MyBluetoothLEDevice).BluetoothLEDevice;
-            LoadGattServices();
-        }
-
-        private void LoadGattServices() {
-            lstGattServices.Items.Clear();
-            string description = string.Empty;
-
-            foreach (var service in currentDevice.GattServices) {
-                description = UUIDHelper.GetUUIDDescription(service.Uuid.ToString());
-                lstGattServices.Items.Add(new MyGattService(description, service));
-            }
-
-        }
-
-        private async void lstGattServices_Tapped(object sender, TappedRoutedEventArgs e) {
-            lstGattServices.Items.Clear();
-            string description = string.Empty;
-
-            foreach (var service in currentDevice.GattServices) {
-                description = UUIDHelper.GetUUIDDescription(service.Uuid.ToString());
-                lstGattServices.Items.Add(new MyGattService(description, service));
-            }
-        }
-
-        private async void lstCharacteristics_Tapped(object sender, TappedRoutedEventArgs e) {
-            lstCharacteristics.Items.Clear();
-            GattDeviceService selectedService = (lstGattServices.SelectedItem as MyGattService).GattDeviceService;
-
-            foreach (var characteristic in selectedService.GetAllCharacteristics()) {
-                lstCharacteristics.Items.Add(characteristic.UserDescription + " " + characteristic.Uuid);
+            catch (Exception ex) {
+                if ((uint)ex.HResult == 0x8007048F) {
+                    //var result = MessageBox.Show(AppResources.Msg_BluetoothOff, "Bluetooth Off", MessageBoxButton.OKCancel);
+                    //if (result == MessageBoxResult.OK) {
+                    //    ShowBluetoothcControlPanel();
+                    //}
+                }
+                else if ((uint)ex.HResult == 0x80070005) {
+                    //MessageBox.Show(AppResources.Msg_MissingCaps);
+                }
+                else {
+                    //MessageBox.Show(ex.Message);
+                }
             }
         }
 
+        async private void ConnectToPeer(Windows.Networking.Proximity.PeerInformation peerInfo) {
+            //WriteMessageText("Peer found. Connecting to " + peerInfo.DisplayName + "\n");
+            try {
+                Windows.Networking.Sockets.StreamSocket socket =
+                    await Windows.Networking.Proximity.PeerFinder.ConnectAsync(peerInfo);
+
+                //WriteMessageText("Connection successful. You may now send messages.\n");
+                this.thingsFound.Text += "Connection successful. You may now send messages.";
+                //SendMessage(socket);
+            }
+            catch (Exception err) {
+                //WriteMessageText("Connection failed: " + err.Message + "\n");
+            }
+        }
+
+        private void doSomething_Click(object sender, RoutedEventArgs e) {
+            // Because I enable the ConnectToSelected button only if the user has
+            // a device selected, I don't need to check here whether that is the case.
+
+            // Connect to the device
+            PeerInformation pdi = _pairedDevices[0];
+
+            // Asynchronous call to connect to the device
+            ConnectToDevice(pdi);
+        }
+
+        private async void ConnectToDevice(PeerInformation peer) {
+            if (_socket != null) {
+                // Disposing the socket with close it and release all resources associated with the socket
+                _socket.Dispose();
+            }
+
+            try {
+                _socket = new StreamSocket();
+                //string serviceName = (String.IsNullOrWhiteSpace(peer.ServiceName)) ? "2" : peer.ServiceName;
+                string serviceName = "3";
+
+                Debug.WriteLine("before first await");
+
+                // Note: If either parameter is null or empty, the call will throw an exception
+                await _socket.ConnectAsync(peer.HostName, serviceName, SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
+
+                MessageDialog messageDialog = new MessageDialog(_socket.Information.RemoteAddress.DisplayName);
+
+                Debug.WriteLine("before");
+                await messageDialog.ShowAsync();
+                //MessageBox.Show(String.Format(AppResources.Msg_ConnectedTo, _socket.Information.RemoteAddress.DisplayName));
+                // If the connection was successful, the RemoteAddress field will be populated
+                //MessageBox.Show(String.Format(AppResources.Msg_ConnectedTo, _socket.Information.RemoteAddress.DisplayName));
+                Debug.WriteLine("poopies hurray");
+            }
+            catch (Exception ex) {
+                // In a real app, you would want to take action dependent on the type of 
+                // exception that occurred.
+                //MessageBox.Show(ex.Message);
+
+                _socket.Dispose();
+                _socket = null;
+            }
+        }
     }
 
-    public class MyBluetoothLEDevice {
-        public BluetoothLEDevice BluetoothLEDevice { get; private set; }
-
-        public MyBluetoothLEDevice(BluetoothLEDevice gattDeviceService) {
-            this.BluetoothLEDevice = gattDeviceService;
+    /// <summary>
+    ///  Class to hold all paired device information
+    /// </summary>
+    public class PairedDeviceInfo {
+        internal PairedDeviceInfo(PeerInformation peerInformation) {
+            this.PeerInfo = peerInformation;
+            this.DisplayName = this.PeerInfo.DisplayName;
+            this.HostName = this.PeerInfo.HostName.DisplayName;
+            this.ServiceName = this.PeerInfo.ServiceName;
         }
 
-        public override string ToString() {
-            return string.Format("{0} ({1})", this.BluetoothLEDevice.Name, this.BluetoothLEDevice.ConnectionStatus.ToString());
-        }
-    }
-
-    public class MyGattService {
-        public GattDeviceService GattDeviceService { get; private set; }
-        public string Description { get; private set; }
-        public MyGattService(string description, GattDeviceService gattDeviceService) {
-            this.Description = description;
-            this.GattDeviceService = gattDeviceService;
-        }
-
-        public override string ToString() {
-            return Description;
-        }
-    }
-
-    public static class UUIDHelper {
-        public static string GetUUIDDescription(string uuid) {
-            switch (uuid) {
-                case "00001811-0000-1000-8000-00805f9b34fb":
-                    return "AlertNotification";
-                case "0000180f-0000-1000-8000-00805f9b34fb":
-                    return "Battery";
-                case "00001810-0000-1000-8000-00805f9b34fb":
-                    return "BloodPressure";
-                case "00001805-0000-1000-8000-00805f9b34fb":
-                    return "CurrentTime";
-                case "00001818-0000-1000-8000-00805f9b34fb":
-                    return "CyclingPower";
-                case "00001816-0000-1000-8000-00805f9b34fb":
-                    return "CyclingSpeedAndCadence";
-                case "0000180a-0000-1000-8000-00805f9b34fb":
-                    return "DeviceInformation";
-                case "00001800-0000-1000-8000-00805f9b34fb":
-                    return "GenericAccess";
-                case "00001801-0000-1000-8000-00805f9b34fb":
-                    return "GenericAttribute";
-                case "00001808-0000-1000-8000-00805f9b34fb":
-                    return "Glucose";
-                case "00001809-0000-1000-8000-00805f9b34fb":
-                    return "HealthThermometer";
-                case "0000180d-0000-1000-8000-00805f9b34fb":
-                    return "HeartRate";
-                case "00001812-0000-1000-8000-00805f9b34fb":
-                    return "HumanInterfaceDevice";
-                case "00001802-0000-1000-8000-00805f9b34fb":
-                    return "ImmediateAlert";
-                case "00001803-0000-1000-8000-00805f9b34fb":
-                    return "LinkLoss";
-                case "00001819-0000-1000-8000-00805f9b34fb":
-                    return "LocationAndNavigation";
-                case "00001807-0000-1000-8000-00805f9b34fb":
-                    return "NextDstChange";
-                case "0000180e-0000-1000-8000-00805f9b34fb":
-                    return "PhoneAlertStatus";
-                case "00001806-0000-1000-8000-00805f9b34fb":
-                    return "ReferenceTimeUpdate";
-                case "00001814-0000-1000-8000-00805f9b34fb":
-                    return "RunningSpeedAndCadence";
-                case "00001813-0000-1000-8000-00805f9b34fb":
-                    return "ScanParameters";
-                case "00001804-0000-1000-8000-00805f9b34fb":
-                    return "TxPower";
-                default:
-                    return "Unknown " + uuid;
-            }
-        }
+        public string DisplayName { get; private set; }
+        public string HostName { get; private set; }
+        public string ServiceName { get; private set; }
+        public PeerInformation PeerInfo { get; private set; }
     }
 }
